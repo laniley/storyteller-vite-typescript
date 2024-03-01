2c0ffd5881bee103d1d6772a0a58ebb6a2f58ecc
Melanie Mende <melanie.mende@me.com>
2024-03-01 15:10:16 +0100

    clean up

--- src/app/routes/routes/ProjectRoute/ProjectRoute.tsx
+++ src/app/routes/routes/ProjectRoute/ProjectRoute.tsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
-import { useAppSelector, useAppDispatch } from './../../hooks'
+import { useAppSelector, useAppDispatch } from './../../../hooks'
 
 import {
 	Colors,
 } from '@blueprintjs/core';
 
-import {
-	Timeline,
-	TopNavBar,
-} from '../../components';
-
-import { getRoute } from '../../store/project/project.selectors';
-import { getBgColor } from '../../store/appState/appState.selectors';
+import { getBgColor } from '../../../store/appState/appState.selectors';
 
 //import { MoveToTrashAlert } from '../../components';
 
@@ -61,11 +54,3 @@ export default function ProjectRoute() {
 		</div>
 	);
 }
-/*
-function mapStateToProps({ appState, project }) {
-	return {
-		route: getRoute(project),
-		border: `1px solid ${appState.theme == 'bp3-dark' ? Colors.DARK_GRAY1 : Colors.LIGHT_GRAY1}`,
-		,
-	};
-}*/
