864ee21349871831fc3eb80fd21b952d39562d17
Melanie Mende <melanie.mende@me.com>
2024-04-09 14:12:41 +0200

    testing project.reducer

---
+++ src/app/store/project/project.reducer.test.ts
@@ -0,0 +1,152 @@
+import reducer, { setCover, setTitle, setAuthor, setAbstract, setDedication, setRoute, setStyles } from './project.reducer'
+import { initialState } from './project.model'
+
+describe('Project reducer', () => {
+
+  it('should return the initial state for an unknown action', () => {
+        expect(reducer(undefined, { type: 'unknown' })).toEqual(initialState)
+	})
+
+	it('should handle setCover', () => {
+		expect(reducer(initialState, setCover('test_cover'))).toEqual(Object.assign({}, initialState, { cover: 'test_cover' }))
+	})
+
+	it('should handle setTitle', () => {
+		expect(reducer(initialState, setTitle('test_title'))).toEqual(Object.assign({}, initialState, { title: 'test_title' }))
+	})
+
+	it('should handle setAuthor', () => {
+		expect(reducer(initialState, setAuthor('firstname lastname'))).toEqual(Object.assign({}, initialState, { author: 'firstname lastname' }))
+	})
+
+	it('should handle setAbstract', () => {
+		expect(reducer(initialState, setAbstract('test'))).toEqual(Object.assign({}, initialState, { abstract: 'test' }))
+	})
+
+	it('should handle setDedication', () => {
+		expect(reducer(initialState, setDedication('test'))).toEqual(Object.assign({}, initialState, { dedication: 'test' }))
+	})
+
+	it('should handle setRoute', () => {
+		expect(reducer(initialState, setRoute('test_route'))).toEqual(Object.assign({}, initialState, { route: 'test_route' }))
+	})
+
+	it('should handle setStyles', () => {
+		expect(reducer(initialState, setStyles('test'))).toEqual(Object.assign({}, initialState, { styles: 'test' }))
+	})
+})
+
+
+/*
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
+
+import * as actions from './project.actions';
+
+jest.mock('fs');
+
+describe('Project actions', () => {
+
+	it('should dispatch SET_COVER action', () => {
+		expect(actions.setCover("cover")).toEqual({
+			cover: 'cover',
+			type: 'SET_COVER'
+		});
+	})
+
+	it('should dispatch SET_TITLE action', () => {
+		expect(actions.setTitle("title")).toEqual({
+			title: 'title',
+			type: 'SET_TITLE'
+		});
+	})
+
+	it('should dispatch SET_AUTHOR action', () => {
+		expect(actions.setAuthor("author")).toEqual({
+			author: 'author',
+			type: 'SET_AUTHOR'
+		});
+	})
+
+	it('should dispatch SET_ABSTRACT action', () => {
+		expect(actions.setAbstract("test")).toEqual({
+			abstract: 'test',
+			type: 'SET_ABSTRACT'
+		});
+	})
+
+	it('should dispatch SET_DEDICATION action', () => {
+		expect(actions.setDedication("dedication")).toEqual({
+			dedication: 'dedication',
+			type: 'SET_DEDICATION'
+		});
+	})
+
+	it('should dispatch SET_SELECTED_CHAPTER action', () => {
+		expect(actions.setSelectedChapter("test")).toEqual({
+			chapter: 'test',
+			type: 'SET_SELECTED_CHAPTER'
+		});
+	})
+
+	it('should dispatch SET_STYLES action', () => {
+		expect(actions.setStyles("test")).toEqual({
+			styles: 'test',
+			type: 'SET_STYLES'
+		});
+	})
+
+	it('should dispatch SET_ROUTE action', () => {
+		expect(actions.setRoute("test")).toEqual({
+			route: 'test',
+			type: 'SET_ROUTE'
+		});
+	})
+
+	/* describe('openProjectAction', () => {
+
+		it('should open a project', () => {
+
+			const MOCK_FILE_INFO = {
+				'/path/to/workspace/project_1': 'project.js',
+				'/path/to/workspace/project_2': 'project.js',
+			};
+
+			require('fs').__setMockFiles(MOCK_FILE_INFO);
+
+			const path_to_workspace = "/path/to/workspace";
+
+			const mockState = {}
+
+			const store = mockStore({
+				workspace: {
+					path: path_to_workspace
+				},
+				appState: {
+					path: '/path/to/workspace/project_2'
+				}
+			})
+
+			store.getState = () => mockState
+
+			const path = path_to_workspace + "/new_project";
+
+			store.dispatch(actions.openProjectAction(path))
+
+			const executed_actions = store.getActions();
+
+			expect(executed_actions).toEqual([
+				{
+					"payload": [
+						{ name: "project_1", path: "/path/to/workspace/project_1", isCurrentlyOpen: false },
+						{ name: "project_2", path: "/path/to/workspace/project_2", isCurrentlyOpen: true }
+					],
+					"type": "WORKSPACE_SET_PROJECTS"
+				}
+			])
+		})
+	}) */
+
+//});
--- src/app/store/project/project.reducer.ts
+++ src/app/store/project/project.reducer.ts
@@ -119,3 +119,77 @@ export const {
 } = actions
 
 export default reducer
+
+
+/*
+import { exportAsEpub } from './project.actions.export.epub.index';
+export { exportAsEpub };
+
+const fs = require('fs-extra');
+
+const path = require('path');
+
+// ############ ACTION TYPES ##############
+export const SET_SELECTED_CHAPTER = 'SET_SELECTED_CHAPTER';
+export const SET_STYLES = 'SET_STYLES';
+
+// ############## ACTIONS #################
+export const setSelectedChapter = (chapter) => ({ type: SET_SELECTED_CHAPTER, chapter });
+export const setStyles = (styles) => ({ type: SET_STYLES, styles });
+
+export const closeProjectAction = () => {
+
+	console.log("closeProjectAction");
+
+	return (dispatch, getState) => {
+
+		storage.get('storyteller', function (error, data) {
+			if (error) throw error;
+
+			if (data) {
+
+				var new_data = Object.assign({}, data, {
+					path: ''
+				});
+
+				storage.set('storyteller', new_data, (error) => {
+					if (error) throw error;
+					dispatch(appStateActions.setPath(""));
+				});
+			}
+		});
+	}
+}
+
+export const archive = () => {
+
+	return (dispatch, getState) => {
+
+		var date = new Date();
+		var date_splitted = date.toISOString().split('T');
+		var date_string = date_splitted[0].replace(/-/g, "") + "_" + date_splitted[1].substring(0, 8).replace(/:/g, "");
+
+		var path = getState().appStateReducer.path;
+
+		fs.copy(path + "/src", path + "/archive/" + date_string, (err) => {
+			if (err) {
+				return console.error(err);
+			}
+			console.log('done!');
+		});
+	};
+}
+
+export const deleteProject = (directoryPath) => {
+
+	return (dispatch, getState) => {
+
+		console.log("deleting project...", directoryPath);
+
+		var rimraf = require("rimraf");
+		rimraf(directoryPath, function () {
+			console.log("done");
+			dispatch(workspaceActions.loadProjects());
+		});
+	};
+}*/
