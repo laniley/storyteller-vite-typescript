442ecd261543b68111825531e90e5329f2afc367
Melanie Mende <melanie.mende@me.com>
2024-04-10 11:29:49 +0200

    test scenes

--- src/app/store/scenes/scenes.reducer.test.ts
+++ src/app/store/scenes/scenes.reducer.test.ts
@@ -1,5 +1,4 @@
 import reducer from './scenes.reducer'
-import * as actions from './scenes.actions'
 import { initialState as initialStateScene } from './scene.model'
 
 const initialState:Scenes = { scenes: [] };
@@ -63,3 +62,75 @@ describe('Scenes reducer', () => {
 	})
 	*/
 })
+/*
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
+
+import * as actions from './scenes.actions';
+
+jest.mock('fs');
+jest.mock('electron-json-storage-sync');
+
+describe('Scenes actions', () => {
+
+	it('should dispatch ADD_SCENE action', () => {
+
+		expect(actions.addScene('scene')).toEqual({
+			scene: 'scene',
+			type: 'ADD_SCENE'
+		});
+	})
+
+	it('should dispatch CREATE_SCENE action', () => {
+
+		expect(actions.createScene('scene')).toEqual({
+			scene: 'scene',
+			type: 'CREATE_SCENE'
+		});
+	})
+
+	describe('deleteScene', () => {
+
+		it('should dispatch SET_DELETED_AT action', () => {
+
+			const mockState = {}
+
+			const store = mockStore({
+			})
+
+			store.getState = () => mockState
+
+			store.dispatch(actions.deleteScene("scene"))
+
+			const executed_actions = store.getActions();
+
+			expect(executed_actions[0].type).toEqual("SET_DELETED_AT")
+			expect(executed_actions[0].scene).toEqual("scene")
+			expect(executed_actions[0].deleted_at.toString().length).toBeGreaterThan(0);
+		})
+	})
+
+/* 	describe('save', () => {
+
+		it('should dispatch save action', () => {
+
+			const mockState = {}
+
+			const store = mockStore({
+				scenes: 'test'
+			})
+
+			store.getState = () => mockState
+
+			require('electron-json-storage-sync').set('storyteller', { "status": true, "data": { "path": "" } });
+
+			store.dispatch(actions.save())
+
+			const executed_actions = store.getActions();
+
+			expect(executed_actions).toEqual({})
+		})
+	}) */
+//})
--- src/app/store/scenes/scenes.reducer.ts
+++ src/app/store/scenes/scenes.reducer.ts
@@ -38,3 +38,108 @@ export const {
 } = actions
 
 export default reducer
+/*
+const fs = require('fs');
+
+// ############ ACTION TYPES ##############
+export const SET_FROM = 'SET_FROM';
+export const SET_TO = 'SET_TO';
+export const SET_LOCATION_ID = 'SET_LOCATION_ID';
+export const SET_TITLE = 'SET_TITLE';
+export const SET_SUBTITLE = 'SET_SUBTITLE';
+export const SET_SUMMARY = 'SET_SUMMARY';
+export const SET_TEXT = 'SET_TEXT';
+export const SET_DELETED_AT = 'SET_DELETED_AT';
+
+// ############## ACTIONS #################
+export const addScene = (scene) => ({ type: ADD_SCENE, scene });
+export const createScene = (scene) => ({ type: CREATE_SCENE, scene });
+
+export const setFrom = (from) => ({ type: SET_FROM, from });
+export const setTo = (to) => ({ type: SET_TO, to });
+export const setLocationId = (locationID) => ({ type: SET_LOCATION_ID, locationID });
+export const setTitle = (title) => ({ type: SET_TITLE, title });
+export const setSubtitle = (subtitle) => ({ type: SET_SUBTITLE, subtitle });
+export const setSummary = (summary) => ({ type: SET_SUMMARY, summary });
+export const setText = (text) => ({ type: SET_TEXT, text });
+export const setDeletedAt = (scene, deleted_at) => ({ type: SET_DELETED_AT, scene, deleted_at });
+
+export const deleteScene = (scene) => {
+
+	return (dispatch, getState) => {
+
+		dispatch(setDeletedAt(scene, new Date()));
+	}
+};
+
+export const save = () => {
+
+	console.log("saving scene...")
+
+	return (dispatch, getState) => {
+
+		let content = JSON.stringify(getState().scenes);
+
+		var result = sync_storage.get('storyteller');
+
+		console.log("result: " + JSON.stringify(result));
+
+		if (result.data.path) {
+
+			fs.writeFile(data.path + "/src/scenes.json", content, (err) => {
+				if (err) {
+					console.log("FAILURE: ", err)
+				}
+				else {
+					console.log("Saved!")
+				}
+			})
+		}
+	};
+};
+
+export const load = (directoryPath) => {
+
+	console.log("load scenes from file: " + directoryPath);
+
+	return (dispatch, getState) => {
+
+		if (!storytellerScenesFileExists(directoryPath + "/src")) {
+			// TO DO: Show UI dialog that directory is not empty, ask user if it should be used for a new project
+			console.log("scenes.json file does not exist");
+		}
+		else {
+			console.log("scenes.json file exists");
+
+			return fs.readFile(directoryPath + '/src/scenes.json', (err, fileData) => {
+
+				if (err) throw err;
+
+				var data = fileData;
+
+				if (!fileData) {
+
+					console.log("scenes.json file exists - but is empty");
+				}
+				else {
+					JSON.parse(data).forEach(scene => {
+						dispatch(addScene(scene));
+					})
+				}
+
+			});
+		}
+	};
+}
+
+function storytellerScenesFileExists(directoryPath) {
+
+	let fileNameExists = false;
+
+	fs.readdirSync(directoryPath).forEach(fileName => {
+		if (fileName == "scenes.json") fileNameExists = true;
+		// console.log(fileName, fileNameExists)
+	});
+
+	return fileNameExists;
+};*/
