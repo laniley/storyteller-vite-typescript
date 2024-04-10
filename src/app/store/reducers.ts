6f2be3611ed1703371b6785bcb1adcc07ac2c16f
Melanie Mende <melanie.mende@me.com>
2024-04-10 17:05:57 +0200

    added characters reducer

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -4,6 +4,7 @@ import { routerReducer } from 'react-router-redux';
 
 import appState from './appState/appState.reducer';
 import chapters from './chapters/chapters.reducer';
+import character from './character/character.reducer';
 import characters from './characters/characters.reducer';
 import partsReducer from './parts/parts.reducer';
 import project from './project/project.reducer';
@@ -13,6 +14,7 @@ import workspace from './workspace/workspace.reducer';
 export const rootReducer = combineReducers({
 	appState,
 	chapters,
+	character,
 	characters,
 	routerReducer,
 	partsReducer,
