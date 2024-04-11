43b0a3068ed1120dece36a526a7ba54951d8f347
Melanie Mende <melanie.mende@me.com>
2024-04-11 08:54:12 +0200

    test ScriptRoute

--- src/app/routes/routes/ProjectRoute/routes/ScriptRoute/ScriptRoute.test.tsx
+++ src/app/routes/routes/ProjectRoute/routes/ScriptRoute/ScriptRoute.test.tsx
@@ -1,12 +1,14 @@
-import { screen, act } from '@testing-library/react'
-import { renderWithProviders } from '../../../../../../utils/test-utils'
+import { waitFor } from '@testing-library/react'
+import { render } from '../../../../../../utils/test-utils'
 import ScriptRoute from './ScriptRoute';
 
 describe('ScriptRoute component', () => {
 
 	it('renders', async() => {
-		await act(async() => renderWithProviders(<ScriptRoute />))
-		expect(screen.getAllByTestId('ScriptRoute').length).toEqual(1);
+    const {getAllById} = render(<ScriptRoute />) 
+		await waitFor(() => {
+			expect(getAllById('ScriptRoute').length).toEqual(1);
+		})
 	});
 
 })
